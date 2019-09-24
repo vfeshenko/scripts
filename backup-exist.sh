55b3904d930b925e16c7b25210b3bfd1c3aacf66
vfeshenko <vad_fesh@mail.ru>
2019-09-24 11:02:16 +0300

    backup

--- backup-exist.sh
+++ backup-exist.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cd /var/lib/vz/dump
-[ -n "$(find vzdump-qemu-$1-*.vma.lzo -type f 2>/dev/null)" ]  && echo -e "\E[32m$(find vzdump-qemu-$1-*.vma.lzo -type f)\E[0m" ||  echo -e "\E[31mNONE\E[0m"
+VM=$1
+if [ ! $1 ]; then
+	VM=100
+fi
+echo $VM
+
+[ -n "$(find vzdump-qemu-$VM-*.vma.lzo -type f 2>/dev/null)" ] && echo -e "\E[32m$(find vzdump-qemu-$1-*.vma.lzo -type f)\E[0m" || echo -e "\E[31mNONE\E[0m"
